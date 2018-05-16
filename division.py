02b31e391f1c5918907d93d74682768bd9124d18
Естифеев Леонид Дмитриевич <l.estifeev@Almaz.local>
2018-05-16 11:38:31 +0300

    change in division.py

--- division.py
+++ division.py
@@ -4,7 +4,7 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         'Divide one number by another')
-    parser.add_argument('numerator', type=float, help='Numerator')
+    passsrser.add_argument('numerator', type=float, help='Numerator')
     parser.add_argument('divisor', type=float, help='Divisor')
     
     args = parser.parse_args()
